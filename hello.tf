31e96cb9c193b34ae2037f016a61d79ee3f29437
Zainabalidev <zaina@Zainab-AQ.localdomain>
2026-04-16 13:25:32 +0300

    Add buckets and local files

--- hello.tf
+++ hello.tf
@@ -1,4 +1,11 @@
 resource "local_file" "hello" {
-  content  = "Hello, this is a file create with Terraform! This is Awesome! "
+  content  = "Hello, this is a file created with Terraform! This is awesome!"
   filename = "${path.module}/hello.txt"
+}
+
+resource "random_pet" "name" {}
+
+resource "local_file" "pet" {
+  content  = "My pet is cute! His name is ${random_pet.name.id}."
+  filename = "${path.module}/pet.txt"
 }
